ddb99079759cdbf0c0e1627910f3089f47cba766
Alexander McMurray <alexgmcm@gmail.com>
2014-06-09 18:03:28 +0100

    wrote load rejection code, need to test and finish
    Signed-off-by: Alexander McMurray <alexgmcm@gmail.com>

--- importdata/loadAndRejectScript.m
+++ importdata/loadAndRejectScript.m
@@ -18,6 +18,37 @@ for i=1:length(filenames)
 
 	else 
 		%code for corrupted data: have to skip over corrupted lines and sew together... 
+		errorLines= cell2mat(errors(cell2mat(cellfun(@(x)strcmp(x,filenames{i}),errors(:,1),'UniformOutput',false)),2));
+
+		%for each entry in error lines we need to go up from1:errorline-1 then errorline+1:nextentry in errorlines-1 etc. 
+		%if no more entry in error lines then go to end
+		clear MEGData;
+		MEGData = [];
+		%get number of lines in file
+		[status, result] = system( ['wc -l ', strcat(dataDir, filename)] );
+		numlinesstr = regexp(result, '(^[0-9]+)?', 'match');
+		numlines = str2num( numlinesstr{1,1} );
+
+		errorLines=[0; errorLines; numlines+1]; %pad error lines for loop
+
+
+		for j=1:(length(errorLines)-1)
+			try
+			MEGData = [MEGData; loadMEGData(strcat(dataDir,filenames{i}),errorLines(j)+1,errorLines(j+1)-1)];
+			catch exception
+				disp(exception.message);
+			end
+		end
+	end
+
+%artefact detection code
+
+
+%rejection code
+
+
+%save file code (use StripFileExtension on filename and strcat to get new extension)
 
 
 
+end
